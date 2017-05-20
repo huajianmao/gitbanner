2648c158d0b45b372b7d271bc5ca9a7201c76717
Huajian Mao <huajianmao@gmail.com>
2017-05-20 17:22:34 +0800

    Add commit command

--- src/3.bitmap2commits.sh
+++ src/3.bitmap2commits.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 echo "Going to create commits according to the bit value of each day"
+
+
+DAYS=20
+COMMIT_TIMES=5
+for i in `seq 1 $COMMIT_TIMES`; do
+  echo 'COMMIT_DATE=`date -v -${DAYS}d` && GIT_AUTHOR_DATE=$COMMIT_DATE GIT_COMMITTER_DATE=$COMMIT_DATE git commit --allow-empty -m "gitbanner" > /dev/null'
+done
